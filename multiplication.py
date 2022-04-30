0e0c3225eaa1bfbe6c1b16cc4c0891547420282f
nishannova <nishan.ali495@gmail.com>
2022-04-30 21:53:10 +0530

    Revert "Addition of new feature called multiplication"
    This reverts commit 97e82bc8b35951eb475e8c06087ef1beb7e1033e.

--- multiplication.py
+++
@@ -1,8 +0,0 @@
-def mult(x, y):
-    """
-    This function is meant for muliplying 2 Integers
-    :params: x: Integer
-    :params: y: Integer
-    :return: Integer: Multiplication of x & y
-    """
-    return x * y
