97e82bc8b35951eb475e8c06087ef1beb7e1033e
nishannova <nishan.ali495@gmail.com>
2022-04-30 21:44:23 +0530

    Addition of new feature called multiplication

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+def mult(x, y):
+    """
+    This function is meant for muliplying 2 Integers
+    :params: x: Integer
+    :params: y: Integer
+    :return: Integer: Multiplication of x & y
+    """
+    return x * y
